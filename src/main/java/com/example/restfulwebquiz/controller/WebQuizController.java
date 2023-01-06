8a8d5f6afb26410f35ef81548160df61798db4a1
nikita <babka1221@gmail.com>
2023-01-06 13:47:48 +0300

    commented rest controllers

--- src/main/java/com/example/restfulwebquiz/controller/WebQuizController.java
+++ src/main/java/com/example/restfulwebquiz/controller/WebQuizController.java
@@ -20,24 +20,50 @@ public class WebQuizController {
     }
 
 
+    /**
+     * API name: Get all quizzes
+     * HTTP method: Get
+     *
+     * @return all existing quizzes
+     * @path api/quizzes
+     */
     @GetMapping("api/quizzes")
     public Iterable<QuizResponse> getQuizzes() {
         return quizService.getQuizzes();
     }
 
 
+    /**
+     * API name: Get quiz by id
+     * HTTP method: Get
+     *
+     * @return quiz obj from given id
+     * @path api/quizzes/{id}
+     */
     @GetMapping("api/quizzes/{id}")
     public ResponseEntity<QuizResponse> getQuizById(@PathVariable("id") int id) {
         return quizService.getQuizById(id);
     }
 
-
+    /**
+     * API name: Create custom quiz
+     * HTTP method: Post
+     *
+     * @return created quiz obj with some properties
+     * @path api/quizzes
+     */
     @PostMapping("api/quizzes")
     public QuizResponse createQuiz(@Valid @RequestBody QuizRequestBody quizRequestBody) {
         return quizService.createQuiz(quizRequestBody);
     }
 
-
+    /**
+     * API name: Solve quiz by its id
+     * HTTP method: Post
+     *
+     * @return wrong or right message if quiz exists and 404 if not
+     * @path api/quizzes/{id}/solve
+     */
     @PostMapping("api/quizzes/{id}/solve")
     public ResponseEntity<QuizSolutionResponse> answerQuiz(@PathVariable("id") int id,
                                                            @RequestBody Map.Entry<String, int[]> answerEntry) {
